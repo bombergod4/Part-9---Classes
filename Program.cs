00898c92295200d313e64e7f2c3d7e4215154c50
Owen Logtenberg <LogtOwen761@GoTVDSB.ca>
2021-03-29 12:18:39 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_9___Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            //Student person = new Student("Joe", "Mama");
+            //Console.WriteLine(person.Firstname);
+            //person.Firstname = "Deez";
+            //Console.WriteLine(person.Firstname);
+            //Console.WriteLine(person);
+            //person.ResetStudentNumber();
+            //Console.WriteLine(person);
+            students.Add(new Student("Deez", "Nuts"));
+            students.Add(new Student("Ben", "Dover"));
+            students.Add(new Student("Candice", "Bereel"));
+
+            for (int i = 0; i < students.Count; i++)
+            {
+                Console.WriteLine(students[i]);
+            }
+            for (int i = 0; i < 2; i ++)
+            {
+                Console.WriteLine("Enter a first name, then a last name");
+                students.Add(new Student (Console.ReadLine(), Console.ReadLine()));
+                Console.WriteLine(students[i+3]);
+            }
+            for (int i = 0; i == students.Count; i ++)
+            {
+                Console.WriteLine(students[i]);
+            }
+               
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_9___Classes
+{
+    public class Student
+    {
+        private static Random generator = new Random();
+        private string firstName;
+        private string lastName;
+        private string email;
+        private int studentNumber;
+        public Student(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            studentNumber = (555000 + generator.Next(0, 1000));
+            GenerateEmail();
+        }
+        public string Firstname
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                this.firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                this.lastName = value;
+            }
+        }
+        public int StudentNumber
+        {
+            get
+            {
+                return studentNumber;
+            }
+
+        }
+        public override string ToString()
+        {
+            return firstName + " " + lastName + "  Student Number: " + studentNumber + "   Email:  " + email;
+        }
+        public void ResetStudentNumber()
+        {
+            studentNumber = generator.Next(0, 999) + 555000;
+        }
+        private void GenerateEmail()
+        {
+            if (firstName.Length < 3 && lastName.Length < 3)
+                email = firstName + lastName + (studentNumber - 555000) +  "@ICS4U.com";
+            else if (firstName.Length < 3)
+                email = firstName + lastName.Substring(0, 3) + (studentNumber - 555000)+ "@ICS4U.com";
+            else if (lastName.Length < 3)
+                email = firstName.Substring(0, 3) + lastName + (studentNumber - 555000) + "@ICS4U.com";
+            else
+                email = firstName.Substring(0, 3) + lastName.Substring(0, 3) + (studentNumber - 555000) + "@ICS4U.com";
+        }
+        
+
+    }
+}
